f1a497f9a23ba01cf09875d59a23e6920fc9c636
nsbt <55523468+nsbt@users.noreply.github.com>
2023-11-17 07:26:38 +0000

    update list_tasks: update response body

--- list_tasks/app.py
+++ list_tasks/app.py
@@ -4,12 +4,11 @@ import boto3
 dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-
+    tasks = [item.get('title').get('S') for item in dynamodb_client.scan(TableName='Tasks')['Items']]
+    
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'tasks': dynamodb_client.scan(
-                TableName='Tasks',
-            )['Items']
+            'tasks': tasks,
         })
     }
